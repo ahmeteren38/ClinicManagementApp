96feb5b82464a19859b5f2a1327e4fce06a6eecf
Ahmet Eren Coşkun <06ahmeteren38@gmail.com>
2025-03-07 17:32:01 +0300

    Employee Add ve Update validasyonları tamamlandı.

---
+++ Core/ClinicManagement.Application/Validations/Employee/AddEmployeeValidation.cs
@@ -0,0 +1,35 @@
+﻿using ClinicManagement.Application.Features.Command.Employee.AddEmloyee;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagement.Application.Validations.Employee
+{
+    public class AddEmployeeValidation : AbstractValidator<AddEmployeeCommandRequest>
+    {
+        public AddEmployeeValidation()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty().WithMessage("Employee name can not be empty!");
+
+            RuleFor(e => e.Surname)
+                .NotEmpty().WithMessage("Employee surname can not be empty!");
+
+            RuleFor(e => e.IdentityNumber)
+                .NotEmpty().WithMessage("Employee identity number can not be empty!").Length(11).WithMessage("Employee identity number must be 11 characters!").Matches(@"^[0-9]{11}$").WithMessage("Employee identity number must have numbers!");
+
+            RuleFor(e => e.Birthday)
+                .NotEmpty().WithMessage("Employee birthday can not be empty!");
+
+            RuleFor(e => e.Job)
+                .NotEmpty().WithMessage("Employee job can not be empty");
+
+            RuleFor(e => e.ClinicId)
+                .NotEmpty().WithMessage("Clinic can not be empty!");
+
+        }
+    }
+}
--- Infrastructure/ClinicManagement.Infrastructure/obj/Debug/net8.0/ClinicManagement.Infrastructure.AssemblyInfo.cs
+++ Infrastructure/ClinicManagement.Infrastructure/obj/Debug/net8.0/ClinicManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043aa7ba713c046c3d857dbafcb9f80e451498f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7837cbb538840e78d29c95dad52008e779f8145c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
